84cb23f13d32731bee88d32e7ba81281eb7c33b6
Ilya <serebr911-1@yandex.ru>
2020-04-20 19:38:40 +0500

    first back up

---
+++ run.py
@@ -0,0 +1,43 @@
+from flask import Flask, url_for, request, render_template, json, redirect
+from HTML_classes import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def main_page():
+    return render_template('main.html')
+
+
+@app.route('/news')
+def news_page():
+    return render_template('news.html')
+
+
+@app.route('/projects')
+def projects_page():
+    return render_template('projects.html')
+
+
+@app.route('/feedback')
+def feedback_page():
+    return render_template('task.html')
+
+
+@app.route('/profile')
+def profile_page():
+    return render_template('task.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    print(form.password)
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
