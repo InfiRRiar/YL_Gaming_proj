e588fe3d70540bffd3910350621d71ed0ec12bbb
Ilya <serebr911-1@yandex.ru>
2020-04-20 21:03:29 +0500

    login page was added

--- HTML_classes.py
+++ HTML_classes.py
@@ -7,4 +7,4 @@ class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    submit = SubmitField('Войти в профиль')
